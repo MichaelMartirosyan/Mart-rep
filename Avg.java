063c63ba6aeff730b655d21ef91c0fc7962660dc
MichaelMartirosyan <Vandior@mail.ru>
2017-10-12 18:04:28 +0300

    Update Avg.java

--- Avg.java
+++ Avg.java
@@ -1,9 +1,12 @@
 public class Avg {
     public static void main(String[] args) {
+        //создаем массив
         double[] s=new double[10];
         double sum=0;
+        //заполнение массива значениями
         for (int i=0;i<s.length;i++)
             s[i]=Math.random()*100;
+        //находим сумму всех элементов массива
         for (double i:s){
             sum+=i;
         }
